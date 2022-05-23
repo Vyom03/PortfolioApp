6a55d6c0845b8fd8b4821338e557b514ecdaca35
Vyom Trivedi <vyom6943@gmail.com>
2022-05-23 14:11:37 -0400

    social media logos added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './index.css';
+import Profile from './PortfolioContainer/Home/Profile';
 
 function App() {
   return (
     <div className="App">
-      
+      <Profile />
     </div>
   );
 }
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+
+            <a href="https://www.linkedin.com/in/vyomtrivedi/">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/Vyom03">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="https://www.facebook.com/vyom.trivedi.71/">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="https://www.instagram.com/vyom_3vedi_/">
+              <i className="fa fa-instagram"></i>
+            </a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
