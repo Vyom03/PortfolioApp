1a87e4d8ff099d6c1c0356c7a36caae064ad4ac1
Vyom Trivedi <vyom6943@gmail.com>
2022-05-23 23:48:41 -0400

    CSS updated

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Typical from 'react-typical'
+import Typical from "react-typical";
+
+import './Profile.css';
 function Profile() {
   return (
     <div className="profile-container">
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-
+              <div className="colz-icon" >
             <a href="https://www.linkedin.com/in/vyomtrivedi/">
               <i className="fa fa-linkedin"></i>
             </a>
@@ -19,51 +21,56 @@ function Profile() {
             <a href="https://www.instagram.com/vyom_3vedi_/">
               <i className="fa fa-instagram"></i>
             </a>
-            
-          </div>
-            
-            <div className="profile-details-name">
-              <span className="priary-text">
-                  {" "}
-                  Hello, I'M <span className="highlighted-text"> Vyom Trivedi</span>
-              </span>
-            </div>
-            <div className="profile-details-role">
-            <span className="priary-text">
-                  {" "}
-                 <h3>
-                    
-                    <Typical
-                        loop={Infinity}
-                        steps={[
-                            "Enthusiastic Dev🔴",
-                            3000,
-                            "Frontend Developer🖥️",
-                            3000,
-                            "FreeLancer🍀",
-                            3000,
-                            "React/ React Native Dev🌟",
-                            3000,
-                        ]}
-                     />
-                 </h3>
-              </span>
-              <span classname='profile-role-tagline'>
-                        I like to learn and implement things.
-              </span>
 
-            </div>
-          <div classsName='profile-options'>
-              <button className='btn primary-btn'>
-                    {""}
-                    Hire Me {" "}
-              </button>
-                        <a href='Vyom Trivedi Resume.pdf' download='Vyom Trivedi Resume.pdf'>
-                            <button className="btn highlighted-btn"> 
-                                Get Resume
-                            </button>
-                        </a>
+              </div>
           </div>
+
+          <div className="profile-details-name">
+            <span className="priary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text"> Vyom Trivedi</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="priary-text">
+              {" "}
+              <h3>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev🔴",
+                    3000,
+                    "Frontend Developer🖥️",
+                    3000,
+                    "FreeLancer🍀",
+                    3000,
+                    "React/React Native Dev🌟",
+                    3000,
+                  ]}
+                />
+              </h3>
+            </span>
+            <span classname="profile-role-tagline">
+              I like to learn and implement things.
+            </span>
+          </div>
+          <div classsName="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a
+              href="Vyom Trivedi Resume.pdf"
+              download="Vyom Trivedi Resume.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+            <div className="profile-picture-background">
+                
+            </div>
         </div>
       </div>
     </div>
